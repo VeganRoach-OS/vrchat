df8dd5e77ee8f9cd7d21910022d604bd98000e91
David McMackins <mcmackinsd@gmail.com>
2014-01-17 20:19:49 -0600

    Fixed emote bug

--- src/base/jdog/Channel.java
+++ src/base/jdog/Channel.java
@@ -73,7 +73,7 @@ public enum Channel
 				{
 					if (x.getLocation().distance(y.getLocation()) <= RANGE)
 					{
-						y.sendMessage(ChatColor.YELLOW + x.getName() + message);
+						y.sendMessage(ChatColor.YELLOW + x.getName() + " " + message);
 						recipients++;
 					}
 				}
