c365b70ed5d6b4030dc08b567a2ffa0cb84321b0
Jordan Giacone <Jmgiacone@gmail.com>
2014-01-19 01:53:02 -0600

    Proofed for redundancy and removed unecessary extra switch case

--- src/base/plugin/ChatHandler.java
+++ src/base/plugin/ChatHandler.java
@@ -1,6 +1,7 @@
 package base.plugin;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -37,75 +38,78 @@ public class ChatHandler extends JavaPlugin
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
     {
-        Channel c;
-		// commands useless in console
+        //A player has to send this command
 		if (sender instanceof Player)
         {
 			Player player = (Player) sender;
+            String name = player.getDisplayName();
+            Channel c;
+
 			// convert command input into lowercase string
 			String command = cmd.getName().toLowerCase();
-			// if no command args, send to channel
-			if (args.length == 0)
-            {
-				switch (command)
-                {
-					case "ooc":
-						c = Channel.OOC;
-						break;
-                    case "say":
-					case "s":
-						c = Channel.SAY;
-						break;
-                    case "yell":
-					case "y":
-						c = Channel.YELL;
-						break;
-                    case "whisper":
-					case "w":
-						c = Channel.WHISPER;
-						break;
-					default:
-                        player.sendMessage(command + " is an invalid chat command. Please try again or type /help VRChat for help.");
-                        return false;
-				}
 
-                players.put(player.getDisplayName(), c);
-                player.sendMessage(c.getColor() + "You've switched to channel: " + c.name().replace('_', ' '));
-			}
-			// else, send a single message in the specified channel
-			else
+            switch (command)
             {
-				String message = "";
-				for (String s : args)
+                case "ooc":
+                    c = Channel.OOC;
+                    break;
+                case "say":
+                case "s":
+                    c = Channel.SAY;
+                    break;
+                case "yell":
+                case "y":
+                    c = Channel.YELL;
+                    break;
+                case "whisper":
+                case "w":
+                    c = Channel.WHISPER;
+                    break;
+                default:
+                    player.sendMessage(ChatColor.RED + command + " is an invalid VRChat command. Please try again or type /help VRChat for help.");
+                    return false;
+            }
+
+            //Check for channel redundancy. No need to switch to a channel the player is already in.
+            if(c != players.get(name))
+            {
+                //No arguments. Send player to specified channel
+                if (args.length == 0)
                 {
-					message += (s + " ");
-				}
-				switch (command)
+
+                    if(c != players.get(name))
+                    {
+                        players.put(name, c);
+                        player.sendMessage(c.getColor() + "You've switched to channel: " + c.name());
+                    }
+                    else
+                    {
+                        player.sendMessage(c.getColor() + "You're already in channel: " + c.name());
+                    }
+                }
+                //Send a single message in the specified channel without switching
+                else
                 {
-					case "ooc":
-						Channel.OOC.sendMessage(player, message);
-						break;
-                    case "say":
-					case "s":
-						Channel.SAY.sendMessage(player, message);
-						break;
-                    case "yell":
-					case "y":
-						Channel.YELL.sendMessage(player, message);
-						break;
-                    case "whisper":
-					case "w":
-						Channel.WHISPER.sendMessage(player, message);
-						break;
-                    case "emote":
-					case "e":
-						Channel.EMOTE.sendMessage(player, message);
-						break;
-				}
-			}
+                    String message = "";
+                    for (String s : args)
+                    {
+                        message += (s + " ");
+                    }
+
+                    c.sendMessage(player, message);
+                }
+            }
+            else
+            {
+                player.sendMessage(ChatColor.RED + "You did not " + (args.length == 0 ? "switch into" : "speak in") + " channel: " + c.name() + " because you're already in it.");
+            }
+            return true;
 		}
+
+        sender.sendMessage("This command cannot be executed via console. Please log into an account and try again.");
 		return false;
 	}
+
     private class PlayerLoginListener implements Listener
     {
         private ChatHandler plugin;
